1c0b4b655a60a0b094a6ade84c351ce96eb9a692
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-10-01 19:23:24 -0300

    Adicionando layout fragment

--- src/main/java/com/api/spring/boot/thymeleaf/springbootthymeleaf/SpringbootThymeleafApplication.java
+++ src/main/java/com/api/spring/boot/thymeleaf/springbootthymeleaf/SpringbootThymeleafApplication.java
@@ -1,7 +1,6 @@
 package com.api.spring.boot.thymeleaf.springbootthymeleaf;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootThymeleafApplication {
