b88eb7b5d5bb03f6f84753b1439c220181e0df38
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-11-02 13:33:16 -0200

    Cadastrar departamento

--- src/main/java/com/api/spring/boot/thymeleaf/springbootthymeleaf/web/controller/DepartamentoController.java
+++ src/main/java/com/api/spring/boot/thymeleaf/springbootthymeleaf/web/controller/DepartamentoController.java
@@ -1,20 +1,33 @@
 package com.api.spring.boot.thymeleaf.springbootthymeleaf.web.controller;
 
+import com.api.spring.boot.thymeleaf.springbootthymeleaf.web.domain.Departamento;
+import com.api.spring.boot.thymeleaf.springbootthymeleaf.web.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    private DepartamentoService departamentoService;
+
     @GetMapping("/cadastrar")
-    public String cadastrar(){
+    public String cadastrar(Departamento departamento){
         return "/departamento/cadastro";
     }
 
     @GetMapping("/listar")
     public String listar(){
         return "/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento){
+        departamentoService.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 }
